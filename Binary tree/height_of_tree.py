130a39fa28558e6f184250a0ee58090c5b69db7f
RAMGOPAL BHADU <64671247+RamgopalBhadu@users.noreply.github.com>
2020-05-02 23:50:00 +0530

    Create height_of_tree.py

---
+++ Binary tree/height_of_tree.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import queue
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def height(root):
+    if root is None:
+        return 0
+    leftcount=height(root.left)
+    rightcount=height(root.right)
+    largest=max(leftcount,rightcount)
+    return 1+largest
+    
+
+def buildLevelTree():
+    
+    root = BinaryTreeNode(int(input()))
+    q = queue.Queue()
+    q.put(root)
+    while not q.empty():
+        currentNode = q.get()
+        leftChild = int(input())
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+        rightChild = int(input())
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+    return root
+
+# Main
+root = buildLevelTree()
+print(height(root))
+
