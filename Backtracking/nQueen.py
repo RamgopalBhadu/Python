b3fd5dc4c4cf04b835f28411be9c3ee6fe455ff7
RAMGOPAL BHADU <64671247+RamgopalBhadu@users.noreply.github.com>
2020-05-02 22:19:56 +0530

    Create nQueen.py

---
+++ Backtracking/nQueen.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def isSafe(row,col,n,board):
+    i=row-1
+    while i>=0:
+        if board[i][col]==1:
+            return False
+        i-=1
+        
+    i=row-1
+    j=col-1
+    while i>=0 and j>=0:
+        if board[i][j]==1:
+            return False
+        i-=1
+        j-=1
+        
+    i=row-1
+    j=col+1
+    while i>=0 and j<n:
+        if board[i][j]==1:
+            return False
+        i-=1
+        j+=1
+    return True
+    
+def printHelper(row,n,board):
+    if row==n:
+        for i in range(n):
+            for j in range(n):
+                print(board[i][j],end=' ')
+        print()
+        return
+    
+    for col in range(n):
+        if isSafe(row,col,n,board):
+            board[row][col]=1
+            printHelper(row+1,n,board)
+            board[row][col]=0
+    return
+    
+def nQueen(n):
+    board=[[0 for j in range(n)] for i in range(n)]
+    printHelper(0,n,board)
+
+
+n = int(input())
+nQueen(n)
+
