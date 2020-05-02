8da748af97d535e3bbe9d4c28b653312953114e3
RAMGOPAL BHADU <64671247+RamgopalBhadu@users.noreply.github.com>
2020-05-02 23:49:00 +0530

    Create nodes_greater+than_x.py

---
+++ Binary tree/nodes_greater+than_x.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import queue
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def countNodesGreaterThanX(root, x):
+    if root is None:
+        return 0
+    countleft=countNodesGreaterThanX(root.left, x)
+    countright=countNodesGreaterThanX(root.right, x)
+    if root.data>x:
+        return 1+countleft+countright
+    else:
+        return countleft+countright
+    pass
+
+def buildLevelTree(levelorder):
+    index = 0
+    length = len(levelorder)
+    if length<=0 or levelorder[0]==-1:
+        return None
+    root = BinaryTreeNode(levelorder[index])
+    index += 1
+    q = queue.Queue()
+    q.put(root)
+    while not q.empty():
+        currentNode = q.get()
+        leftChild = levelorder[index]
+        index += 1
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+        rightChild = levelorder[index]
+        index += 1
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+    return root
+
+# Main
+levelOrder = [int(i) for i in input().strip().split()]
+x=int(input())
+root = buildLevelTree(levelOrder)
+print(countNodesGreaterThanX(root, x))
+
