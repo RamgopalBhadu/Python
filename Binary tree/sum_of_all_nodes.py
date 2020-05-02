8c1326ab9e8d251825050e22c2557ea07566cbeb
RAMGOPAL BHADU <64671247+RamgopalBhadu@users.noreply.github.com>
2020-05-02 23:46:12 +0530

    Create sum_of_all_nodes.py

---
+++ Binary tree/sum_of_all_nodes.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import queue
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def sumOfAllNodes(root):
+    if root is None:
+        return 0
+    sum1=sumOfAllNodes(root.left)
+    sum2=sumOfAllNodes(root.right)
+    return sum1+sum2+root.data
+    pass
+
+def buildLevelTree(levelorder):
+    index = 0
+    length = len(levelorder)
+    if length<=0 or levelorder[0]==-1:
+        return None
+    root = BinaryTreeNode(levelorder[index])
+    index += 1
+    q = queue.Queue()
+    q.put(root)
+    while not q.empty():
+        currentNode = q.get()
+        leftChild = levelorder[index]
+        index += 1
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+        rightChild = levelorder[index]
+        index += 1
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+    return root
+
+# Main
+levelOrder = [int(i) for i in input().strip().split()]
+root = buildLevelTree(levelOrder)
+print(sumOfAllNodes(root))
+
