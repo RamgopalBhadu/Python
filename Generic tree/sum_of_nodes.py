3602e043bbb44464254823304ee1ccc74975cd60
RAMGOPAL BHADU <64671247+RamgopalBhadu@users.noreply.github.com>
2020-05-02 23:27:51 +0530

    Create sum_of_nodes.py

---
+++ Generic tree/sum_of_nodes.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class treeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+    def __str__(self):
+        return str(self.data)
+
+def sumofNodes(tree):
+    sum=tree.data
+    for child in tree.children:
+        sum=sumofNodes(child)+sum
+    return sum    
+    pass
+
+def createLevelWiseTree(arr):
+    root = treeNode(int(arr[0]))
+    q = [root]
+    size = len(arr)
+    i = 1
+    while i<size:
+        parent = q.pop(0)
+        childCount = int(arr[i])
+        i += 1
+        for j in range(0,childCount):
+            temp = treeNode(int(arr[i+j]))
+            parent.children.append(temp)
+            q.append(temp)
+        i += childCount
+    return root
+
+# Main
+arr = list(int(x) for x in input().strip().split(' '))
+tree = createLevelWiseTree(arr)
+print(sumofNodes(tree))
+
