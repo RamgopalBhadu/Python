ba23e6fc7ced25d52ed539c7f12160bcdfe4cef7
RAMGOPAL BHADU <64671247+RamgopalBhadu@users.noreply.github.com>
2020-05-03 11:46:39 +0530

    Create class_linked_list.py

---
+++ Linked list/class_linked_list.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def length(head):
+    count=0
+    while head is not None:
+        count=count+1
+        head=head.next
+    return count    
+    pass
+
+def ll(arr):
+    if len(arr)==0:
+        return None
+    head = Node(arr[0])
+    last = head
+    for data in arr[1:]:
+        last.next = Node(data)
+        last = last.next
+    return head
+
+# Main
+# Read the link list elements including -1
+arr=list(int(i) for i in input().strip().split(' '))
+# Create a Linked list after removing -1 from list
+l = ll(arr[:-1])
+len=length(l)
+print(len)
+
