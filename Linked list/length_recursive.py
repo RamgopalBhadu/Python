5ab6ef1d8cf4eef9fd8d054c29346495a0223e0d
RAMGOPAL BHADU <64671247+RamgopalBhadu@users.noreply.github.com>
2020-05-03 11:50:37 +0530

    Create length_recursive.py

---
+++ Linked list/length_recursive.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def lengthRecursive(head):
+    if head is None:
+        return 0
+    return 1+lengthRecursive(head.next)
+    pass
+
+def ll(arr):
+    if len(arr)==0:
+        return None
+    head = Node(arr[0])
+    last = head
+    for data in arr[1:]:
+        last.next = Node(data)
+        last = last.next
+    return head
+
+# Main
+from sys import setrecursionlimit
+setrecursionlimit(11000)
+# Read the link list elements including -1
+arr=list(int(i) for i in input().strip().split(' '))
+# Create a Linked list after removing -1 from list
+l = ll(arr[:-1])
+len=lengthRecursive(l)
+print(len)
+
