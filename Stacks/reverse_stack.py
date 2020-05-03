8dda46ca018388a72e8688fb4012335432170d44
RAMGOPAL BHADU <64671247+RamgopalBhadu@users.noreply.github.com>
2020-05-03 06:50:06 +0530

    Create reverse_stack.py

---
+++ Stacks/reverse_stack.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+def reverseStack(s1,s2):
+    if len(s1)<=1:
+        return
+    while(len(s1)!=1):
+        ele=s1.pop()
+        s2.append(ele)
+        
+    lastelement=s1.pop()
+    while(len(s2)!=0):
+        ele=s2.pop()
+        s1.append(ele)   
+    reverseStack(s1,s2)    
+    s1.append(lastelement)  
+#### Implement the code here
+
+from sys import setrecursionlimit
+setrecursionlimit(11000)
+n = int(input())
+s1 = [int(ele) for ele in input().split()]
+s2 = []
+reverseStack(s1,s2)
+while(len(s1) !=0):
+    print(s1.pop(),end= ' ')
+
+
+
+
